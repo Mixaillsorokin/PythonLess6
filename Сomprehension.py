49a9a28c0ca33785977a3b09a8679f376d07fba7
Sorokin_Mixail <111079051+Mixaillsorokin@users.noreply.github.com>
2022-11-14 22:15:17 +0300

    Update Сomprehension.py

--- Сomprehension.py
+++ Сomprehension.py
@@ -5,6 +5,6 @@
 #         if(i % i==0)and(i%1==0):
 #             numlist.append(i)
 # print(f'Простые множетели N-го числа => {numlist}')
-
+______________________________________________________________
 numlist = [i  for i in range(1,int(input('N :'))) if N % i==0 if(i % i==0)and(i%1==0)]
 print(f'Простые множетели N-го числа => {numlist}')
