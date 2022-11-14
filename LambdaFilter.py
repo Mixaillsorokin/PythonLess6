5cacfbedeb5a0607fde558034ffd187c629d4b1c
Sorokin_Mixail <111079051+Mixaillsorokin@users.noreply.github.com>
2022-11-14 22:15:43 +0300

    Update LambdaFilter.py

--- LambdaFilter.py
+++ LambdaFilter.py
@@ -5,7 +5,7 @@
 #     if 'абв' in list[i]:
 #         list.remove(list[i])
 # print(list)
-
+__________________________________________________________________
 text = ('лырабв абв вова авбфцл лыфабвлыо абвнцш').split(' ')
 text2 = list(filter(lambda i:'абв' not in i, text))
 print(text2)
